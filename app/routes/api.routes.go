d4d82b3eec031aedcc9962d012ca478aa2d0dd79
Wahid Fattahillah <35924408+WahidFatahilah@users.noreply.github.com>
2025-01-10 10:31:02 +0700

    add upload feature

--- app/routes/api.routes.go
+++ app/routes/api.routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	api_department_controller "triesdi/app/controllers/api/department_controller"
 	api_home_controller "triesdi/app/controllers/api/home_controller"
+	"triesdi/app/controllers/v1/v1_upload_controller"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,5 +14,6 @@ func InitApiRoute(app *gin.Engine) {
 	api := route.Group("/api")
 
 	api.POST("/department", api_department_controller.CreateDepartment)
+	route.POST("/v1/upload", v1_upload_controller.UploadImage)
 	api.GET("/", api_home_controller.Index)
 }
